3c66c56125f46930bc9398b8395dbfde3fe2c5a0
qthequartermasterman <qthequartermasterman@gmail.com>
2021-11-30 14:30:10 -0600

    Client Code Quality improvements

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -249,27 +249,6 @@ function setupSocket(socket) {
         window.chat.addSystemLine('{GAME} - <b>' + (data.name.length < 1 ? 'An unnamed tank' : data.name) + '</b> joined.');
     });
 
-    // socket.on('leaderboard', function (data) {
-    //     leaderboard = data.leaderboard;
-    //     var status = '<span class="title">Leaderboard</span>';
-    //     for (var i = 0; i < leaderboard.length; i++) {
-    //         status += '<br />';
-    //         if (leaderboard[i].id == player.id){
-    //             if(leaderboard[i].name.length !== 0)
-    //                 status += '<span class="me">' + (i + 1) + '. ' + leaderboard[i].name + "</span>";
-    //             else
-    //                 status += '<span class="me">' + (i + 1) + ". An unnamed tank</span>";
-    //         } else {
-    //             if(leaderboard[i].name.length !== 0)
-    //                 status += (i + 1) + '. ' + leaderboard[i].name;
-    //             else
-    //                 status += (i + 1) + '. An unnamed tank';
-    //         }
-    //     }
-    //     //status += '<br />Players: ' + data.players;
-    //     document.getElementById('status').innerHTML = status;
-    // });
-
     socket.on('serverMSG', function (data) {
         window.chat.addSystemLine(data);
     });
@@ -295,7 +274,7 @@ function setupSocket(socket) {
             //console.log(data)
             let planet;
             for (let j = 0; j < planets.length; j++){
-                if (planets[j].id == data.id){
+                if (planets[j].id === data.id){
                     planet = planets[j];
                 }
             }
@@ -305,7 +284,7 @@ function setupSocket(socket) {
             }
         }
 
-        else if (data.sprite == 'SpriteType.BULLET_SPRITE') {
+        else if (data.sprite === 'SpriteType.BULLET_SPRITE') {
             //console.log('got bullet')
             bullets.push(data)
         } else {
@@ -320,11 +299,11 @@ function setupSocket(socket) {
         //console.log('Receiving tank updates', Date.now())
         users = tankList.filter((t)=>{return typeof(t.sprite) != 'undefined'});
         for (let i = 0; i < users.length; i++) {
-            if (users[i].id == socket.id) {
+            if (users[i].id === socket.id) {
                 //console.log('found player')
                 let health_inventory_changes = false;
-                var xoffset = player.x - users[i].x;
-                var yoffset = player.y - users[i].y;
+                const xoffset = player.x - users[i].x;
+                const yoffset = player.y - users[i].y;
 
                 player.x = users[i].x;
                 player.y = users[i].y;
@@ -358,31 +337,27 @@ function setupSocket(socket) {
     })
     // Handle movement.
     socket.on('-serverTellPlayerMove', function (data) {
-        userData = data[0];
-        foodsList = data[1];
-        massList = data[2];
-        virusList = data[3];
-        var playerData;
-        for(var i =0; i< userData.length; i++) {
+        let userData = data[0];
+        let foodsList = data[1];
+        let massList = data[2];
+        let virusList = data[3];
+        let playerData;
+        for(let i =0; i< userData.length; i++) {
             if(typeof(userData[i].id) == "undefined") {
                 playerData = userData[i];
                 i = userData.length;
             }
         }
-        if(global.playerType == 'player') {
-            var xoffset = player.x - playerData.x;
-            var yoffset = player.y - playerData.y;
+        if(global.playerType === 'player') {
+            const xoffset = player.x - playerData.x;
+            const yoffset = player.y - playerData.y;
 
             player.x = playerData.x;
             player.y = playerData.y;
             player.hue = playerData.hue;
-            //player.massTotal = playerData.massTotal;
-            //player.cells = playerData.cells;
             player.xoffset = isNaN(xoffset) ? 0 : xoffset;
             player.yoffset = isNaN(yoffset) ? 0 : yoffset;
-
         }
-        //users = userData;
         foods = foodsList;
         viruses = virusList;
         fireFood = massList;
@@ -438,15 +413,6 @@ function drawCircle(centerX, centerY, radius, sides) {
     graph.closePath();
     graph.stroke();
     graph.fill();
-}
-
-function drawFood(food) {
-    graph.strokeStyle = 'hsl(' + food.hue + ', 100%, 45%)';
-    graph.fillStyle = 'hsl(' + food.hue + ', 100%, 50%)';
-    graph.lineWidth = foodConfig.border;
-    drawCircle(food.x - player.x + global.screenWidth / 2,
-               food.y - player.y + global.screenHeight / 2,
-               food.radius, global.foodSides);
 }
 
 function rotateAndDrawImage(context, image, angleInRadians, positionX, positionY, axisX=0, axisY=0, imageWidth=1, imageHeight=1){
@@ -636,127 +602,6 @@ function drawBullet(bullet){
     rotateAndDrawImage(graph, sprites[sprite_name], bullet.roll, centerX, centerY,0,0)
 }
 
-function drawVirus(virus) {
-    graph.strokeStyle = virus.stroke;
-    graph.fillStyle = virus.fill;
-    graph.lineWidth = virus.strokeWidth;
-    drawCircle(virus.x - player.x + global.screenWidth / 2,
-               virus.y - player.y + global.screenHeight / 2,
-               virus.radius, global.virusSides);
-}
-
-function drawFireFood(mass) {
-    graph.strokeStyle = 'hsl(' + mass.hue + ', 100%, 45%)';
-    graph.fillStyle = 'hsl(' + mass.hue + ', 100%, 50%)';
-    graph.lineWidth = playerConfig.border+10;
-    drawCircle(mass.x - player.x + global.screenWidth / 2,
-               mass.y - player.y + global.screenHeight / 2,
-               mass.radius-5, 18 + (~~(mass.masa/5)));
-}
-
-function drawPlayers(order) {
-    var start = {
-        x: player.x - (global.screenWidth / 2),
-        y: player.y - (global.screenHeight / 2)
-    };
-
-    for(var z=0; z<order.length; z++)
-    {
-        var userCurrent = users[order[z].nCell];
-        var cellCurrent = users[order[z].nCell];
-
-        var x=0;
-        var y=0;
-
-        var points = 30 + ~~(cellCurrent.mass/5);
-        var increase = Math.PI * 2 / points;
-
-        graph.strokeStyle = 'hsl(' + userCurrent.hue + ', 100%, 45%)';
-        graph.fillStyle = 'hsl(' + userCurrent.hue + ', 100%, 50%)';
-        graph.lineWidth = playerConfig.border;
-
-        var xstore = [];
-        var ystore = [];
-
-        global.spin += 0.0;
-
-        var circle = {
-            x: cellCurrent.x - start.x,
-            y: cellCurrent.y - start.y
-        };
-
-        for (var i = 0; i < points; i++) {
-
-            x = cellCurrent.radius * Math.cos(global.spin) + circle.x;
-            y = cellCurrent.radius * Math.sin(global.spin) + circle.y;
-            if(typeof(userCurrent.id) == "undefined") {
-                x = valueInRange(-userCurrent.x + global.screenWidth / 2,
-                                 global.gameWidth - userCurrent.x + global.screenWidth / 2, x);
-                y = valueInRange(-userCurrent.y + global.screenHeight / 2,
-                                 global.gameHeight - userCurrent.y + global.screenHeight / 2, y);
-            } else {
-                x = valueInRange(-cellCurrent.x - player.x + global.screenWidth / 2 + (cellCurrent.radius/3),
-                                 global.gameWidth - cellCurrent.x + global.gameWidth - player.x + global.screenWidth / 2 - (cellCurrent.radius/3), x);
-                y = valueInRange(-cellCurrent.y - player.y + global.screenHeight / 2 + (cellCurrent.radius/3),
-                                 global.gameHeight - cellCurrent.y + global.gameHeight - player.y + global.screenHeight / 2 - (cellCurrent.radius/3) , y);
-            }
-            global.spin += increase;
-            xstore[i] = x;
-            ystore[i] = y;
-        }
-        /*if (wiggle >= player.radius/ 3) inc = -1;
-        *if (wiggle <= player.radius / -3) inc = +1;
-        *wiggle += inc;
-        */
-        for (i = 0; i < points; ++i) {
-            if (i === 0) {
-                graph.beginPath();
-                graph.moveTo(xstore[i], ystore[i]);
-            } else if (i > 0 && i < points - 1) {
-                graph.lineTo(xstore[i], ystore[i]);
-            } else {
-                graph.lineTo(xstore[i], ystore[i]);
-                graph.lineTo(xstore[0], ystore[0]);
-            }
-
-        }
-        graph.lineJoin = 'round';
-        graph.lineCap = 'round';
-        graph.fill();
-        graph.stroke();
-        var nameCell = "";
-        if(typeof(userCurrent.id) == "undefined")
-            nameCell = player.name;
-        else
-            nameCell = userCurrent.name;
-
-        var fontSize = Math.max(cellCurrent.radius / 3, 12);
-        graph.lineWidth = playerConfig.textBorderSize;
-        graph.fillStyle = playerConfig.textColor;
-        graph.strokeStyle = playerConfig.textBorder;
-        graph.miterLimit = 1;
-        graph.lineJoin = 'round';
-        graph.textAlign = 'center';
-        graph.textBaseline = 'middle';
-        graph.font = 'bold ' + fontSize + 'px sans-serif';
-
-        if (global.toggleMassState === 0) {
-            graph.strokeText(nameCell, circle.x, circle.y);
-            graph.fillText(nameCell, circle.x, circle.y);
-        } else {
-            graph.strokeText(nameCell, circle.x, circle.y);
-            graph.fillText(nameCell, circle.x, circle.y);
-            graph.font = 'bold ' + Math.max(fontSize / 3 * 2, 10) + 'px sans-serif';
-            if(nameCell.length === 0) fontSize = 0;
-            graph.strokeText(Math.round(cellCurrent.mass), circle.x, circle.y+fontSize);
-            graph.fillText(Math.round(cellCurrent.mass), circle.x, circle.y+fontSize);
-        }
-    }
-}
-
-function valueInRange(min, max, value) {
-    return Math.min(max, Math.max(min, value));
-}
 
 function drawgrid() {
      graph.lineWidth = 1;
